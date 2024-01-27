30a5063a7ca9efb1261f7fa9274d25d930cb1212
Saleem Malik <roxsaleemmalik1@gmail.com>
2024-01-27 23:42:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+<<<<<<< Updated upstream
 // import reportWebVitals from "./reportWebVitals";
+=======
+import reportWebVitals from "./reportWebVitals";
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
