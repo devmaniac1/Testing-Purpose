5889a54d39c99b2f49cbf79f66aa6112832da3f5
Ashfaq Farleen <139785309+devmaniac1@users.noreply.github.com>
2024-01-27 20:09:11 +0530

    Added a Footer bar on mobile view

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import "./App.css";
 import Itinerary from "./Itinerary.js";
 import Navbar from "./Navbar.js";
 import Map from "./Map.js";
+import FooterBar from "./FooterBar.js";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,8 +28,9 @@ function App() {
       </header>
       <div className="main_app">
         <Itinerary />
-        <Map />
+        {viewportWidth > 800 && <Map />}
       </div>
+      {viewportWidth <= 800 && <FooterBar />}
     </div>
   );
 }
