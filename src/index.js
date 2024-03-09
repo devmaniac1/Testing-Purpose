c36aa2e1132ef688841ff82a8fdf682452ce65bd
Saleem Malik <roxsaleemmalik1@gmail.com>
2024-03-09 10:25:08 +0530

    Revert "Added Analytics to project"
    This reverts commit 97aaf42b7ca956a2f9dcb14bfe6ef961a0e941ac.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
